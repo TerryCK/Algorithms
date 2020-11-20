d61f28de64b7f24f059dcea696494c50c1224a8d
Terry Chen <vul354@gmail.com>
2020-11-20 18:23:38 +0800

    Add 232. Implement Queue using Stacks

--- README.md
+++ README.md
@@ -13,3 +13,4 @@ this repo is for personally learning notes.
 * [155. Min Stack](../main/solutions/0155_Min_Stack.md)
 * [0020 Valid Parentheses](../main/solutions/0020-Valid_Parentheses.md)
 * [0225 Implement Stack using Queues](../main/solutions/0225-Implement%20Stack%20using%20Queues.md)
+* [232. Implement Queue using Stacks](../main/solutions/solutions/0232-Implement%20Queue%20using%20Stacks.md)
---
+++ playgrounds/232. Implement Queue using Stacks.playground/Contents.swift
@@ -0,0 +1,90 @@
+/*
+ ### Implement Queue using Stacks
+ 
+ Implement the following operations of a queue using stacks.
+ 
+ * __push(x)__ -- Push element x to the back of queue.
+ * __pop()__ -- Removes the element from in front of queue.
+ * __peek()__ -- Get the front element.
+ * __empty()__ -- Return whether the queue is empty.
+ 
+ __Example:__
+ ```
+ MyQueue queue = new MyQueue();
+ 
+ queue.push(1);
+ queue.push(2);
+ queue.peek();  // returns 1
+ queue.pop();   // returns 1
+ queue.empty(); // returns false
+ ```
+ 
+ __Notes:__
+ * You must use only standard operations of a stack -- which means only `push to top`,  `peek/pop from top`, `size`, and `is empty` operations are valid.
+ * Depending on your language, stack may not be supported natively. You may simulate a stack by using a list or deque (double-ended queue), as long as you use only standard operations of a stack.
+ * You may assume that all operations are valid (for example, no pop or peek operations will be called on an empty queue).
+ */
+
+struct Stack<Element> {
+    private var storage = Array<Element>()
+    
+    mutating func push(_ element: Element) {
+        storage.append(element)
+    }
+    
+    mutating func pop() -> Element? {
+        storage.popLast()
+    }
+    
+    var isEmpty: Bool {
+        storage.isEmpty
+    }
+    
+    var top: Element? {
+        storage.last
+    }
+    
+    var size: Int {
+        storage.count
+    }
+}
+
+
+
+struct Queue<Element> {
+    private var storage = Stack<Element>()
+    // O(1)
+    var peek: Element? {
+        storage.top
+    }
+    // O(1)
+    mutating func dequeue() -> Element? {
+        storage.pop()
+    }
+    // O(1)
+    var isEmpty: Bool {
+        peek == nil
+    }
+    
+    // Space O(n), Time: O(n + n)
+    mutating func enqueue(_ element: Element) {
+        var stack = Stack<Element>()
+        while let pop = storage.pop() {
+            stack.push(pop)
+        }
+        stack.push(element)
+        
+        while let pop = stack.pop() {
+            storage.push(pop)
+        }
+    }
+}
+
+var queue = Queue<Int>()
+
+queue.enqueue(1)
+queue.peek
+queue.enqueue(2)
+queue.peek
+queue.dequeue()
+queue.dequeue()
