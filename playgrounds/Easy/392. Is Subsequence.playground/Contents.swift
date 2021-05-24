9ecc6f1afd60858a6e4a565a8828d631e89620ff
Terry Chen <vul354@gmail.com>
2021-05-24 17:30:53 +0800

    Add 392. Is Subsequence

---
+++ playgrounds/Easy/392. Is Subsequence.playground/Contents.swift
@@ -0,0 +1,46 @@
+/*
+ Given two strings s and t, return true if s is a subsequence of t, or false otherwise.
+
+ A subsequence of a string is a new string that is formed from the original string by deleting some (can be none) of the characters without disturbing the relative positions of the remaining characters. (i.e., "ace" is a subsequence of "abcde" while "aec" is not).
+
+  
+
+ Example 1:
+
+ Input: s = "abc", t = "ahbgdc"
+ Output: true
+ Example 2:
+
+ Input: s = "axc", t = "ahbgdc"
+ Output: false
+  
+
+ Constraints:
+
+ 0 <= s.length <= 100
+ 0 <= t.length <= 104
+ s and t consist only of lowercase English letters.
+  
+
+ Follow up: Suppose there are lots of incoming s, say s1, s2, ..., sk where k >= 109, and you want to check one by one to see if t has its subsequence. In this scenario, how would you change your code?
+ */
+
+class Solution {
+    func isSubsequence(_ s: String, _ t: String) -> Bool {
+        let s = Array(s), t = Array(t)
+        var slow = 0, f = 0
+        while f < t.count, slow < s.count {
+            if s[slow] == t[f] {
+                slow += 1
+            }
+            f += 1
+        }
+        return slow == s.count
+    }
+}
+
+
+let solution = Solution()
+solution.isSubsequence("abc", "ahbgdc") == true
+solution.isSubsequence("axc", "ahbgdc") == false
+solution.isSubsequence("", "ahbgdc")    == true
